87fa5529c9978333afd76c6fdec01b2409365281
angelo-squadra <angelo.savioti@squadra.com.br>
2022-06-12 05:49:12 -0300

    created infrastructure folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_favorite_games/app/app.dart';
-import 'package:my_favorite_games/service_locator.dart';
+import 'package:my_favorite_games/app/infrastructure/service_locator.dart';
 
 void main() async {
   await initializeServiceLocator();
