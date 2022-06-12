0c3af1641627187c15cc516734cce16e6790f402
angelo-squadra <angelo.savioti@squadra.com.br>
2022-06-12 03:31:23 -0300

    registered Dio in the service locator

---
+++ lib/env.dart
@@ -0,0 +1,3 @@
+class Env {
+  static const String baseUrl = 'https://www.cheapshark.com/api/1.0/';
+}
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:my_favorite_games/env.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final serviceLocator = GetIt.instance;
@@ -25,4 +27,16 @@ Future<void> _initExternalDependencies() async {
       () async => await SharedPreferences.getInstance());
   serviceLocator.registerLazySingleton<InternetConnectionChecker>(
       () => InternetConnectionChecker());
+  serviceLocator.registerLazySingleton<Dio>(
+    () => Dio(
+      BaseOptions(
+        baseUrl: Env.baseUrl,
+        connectTimeout: 10000,
+        receiveTimeout: 10000,
+        validateStatus: (status) {
+          return status != null && status <= 402;
+        },
+      ),
+    ),
+  );
 }
