5b824b5ecc5be1a85b9ec7645c36c09def707a2f
angelo-squadra <angelo.savioti@squadra.com.br>
2022-06-17 00:36:45 -0300

    added initialization ensuring

--- lib/app/infrastructure/dependency_injection/service_locator.dart
+++ lib/app/infrastructure/dependency_injection/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
@@ -9,6 +10,7 @@ import 'package:path_provider/path_provider.dart';
 final serviceLocator = GetIt.instance;
 
 Future<void> initializeServiceLocator() async {
+  WidgetsFlutterBinding.ensureInitialized();
   _initCore();
   _initFeatures();
   _initRepositories();
