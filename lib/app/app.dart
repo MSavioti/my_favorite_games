d69497b43bc82de956eb7985a20f5f14e3b3dde2
angelo-squadra <angelo.savioti@squadra.com.br>
2022-06-18 00:26:21 -0300

    created routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_favorite_games/app/modules/game/presentation/screens/game_module.dart';
 import 'package:my_favorite_games/app/modules/home/presentation/screens/home_screen.dart';
-import 'package:my_favorite_games/app/modules/search/presentation/screens/search_screen.dart';
+import 'package:my_favorite_games/app/modules/search/presentation/screens/search_module.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -8,8 +9,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/game': (context) => const GameModule(),
+        '/search': (context) => const SearchModule(),
+      },
     );
   }
 }
