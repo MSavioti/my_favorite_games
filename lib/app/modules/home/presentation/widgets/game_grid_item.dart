90f2d0afeb112dfffed51b792e981043eda6fbcd
angelo-squadra <angelo.savioti@squadra.com.br>
2022-06-30 18:17:37 -0300

    added shadow to better display a grid item text

--- lib/app/modules/home/presentation/widgets/game_grid_item.dart
+++ lib/app/modules/home/presentation/widgets/game_grid_item.dart
@@ -6,32 +6,56 @@ class GameGridItem extends StatelessWidget {
     Key? key,
     required this.game,
     required this.onTap,
+    required this.availableWidth,
   }) : super(key: key);
 
   final Game game;
   final Function() onTap;
+  final double availableWidth;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        color: Colors.grey[900],
-        padding: const EdgeInsets.all(8.0),
+    return Container(
+      color: Colors.grey[900],
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: onTap,
         child: Stack(
           children: [
             Positioned.fill(
               child: Image.network(game.thumbnail),
             ),
             Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              height: 64.0,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black,
+                      Colors.transparent,
+                    ],
+                    stops: [0.05, 0.75],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
               bottom: 4.0,
               child: SizedBox(
+                width: availableWidth,
                 child: Text(
                   game.externalName,
                   style: const TextStyle(
                     color: Colors.white70,
-                    fontSize: 12.0,
+                    fontSize: 10.0,
                   ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
               ),
             ),
--- lib/app/modules/home/presentation/widgets/grid_favorited_games.dart
+++ lib/app/modules/home/presentation/widgets/grid_favorited_games.dart
@@ -5,6 +5,9 @@ import 'package:my_favorite_games/app/shared/entities/game.dart';
 class GridFavoriteGames extends StatelessWidget {
   final List<Game> games;
   final Function() onRefresh;
+  final int columnsCount = 3;
+  final double padding = 16.0;
+  final double gridItemAspectRatio = 3 / 4;
 
   const GridFavoriteGames({
     Key? key,
@@ -14,17 +17,21 @@ class GridFavoriteGames extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double gridItemWidth = screenWidth * 0.33 - padding * 4;
+
     return Flexible(
       child: GridView.count(
-        crossAxisCount: 3,
-        crossAxisSpacing: 16.0,
-        mainAxisSpacing: 16.0,
-        childAspectRatio: 3 / 4,
+        crossAxisCount: columnsCount,
+        crossAxisSpacing: padding,
+        mainAxisSpacing: padding,
+        childAspectRatio: gridItemAspectRatio,
         shrinkWrap: true,
         children: [
           for (int i = 0; i < games.length; i++)
             GameGridItem(
               game: games[i],
+              availableWidth: gridItemWidth,
               onTap: () async => await Navigator.of(context)
                   .pushNamed(
                     '/game',
